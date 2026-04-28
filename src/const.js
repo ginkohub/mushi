72aa3c6ad35878f9e29370619bffeeaa0a384376
ginkohub <ginkohub@gmail.com>
2026-04-28 22:13:06 +0800

    chore: update event constants

--- src/const.js
+++ src/const.js
@@ -8,58 +8,69 @@
  * This code is part of Ginko project (https://github.com/ginkohub)
  */
 
-export const CREDS_UPDATE = 'creds.update';
-
 export const CONNECTION_UPDATE = 'connection.update';
-
+export const CREDS_UPDATE = 'creds.update';
 export const MESSAGING_HISTORY_SET = "messaging-history.set";
-
-export const PRESENCE_UPDATE = 'presence.update';
-
-export const MESSAGES_UPSERT = 'messages.upsert';
-export const MESSAGES_UPDATE = 'messages.update';
-export const MESSAGES_DELETE = 'messages.delete';
-export const MESSAGES_REACTION = 'messages.reaction';
-export const MESSAGE_RECEIPT_UPDATE = 'message-receipt.update';
-
 export const CHATS_UPSERT = 'chats.upsert';
-export const CHATS_DELETE = 'chats.delete';
 export const CHATS_UPDATE = 'chats.update';
-
-export const BLOCKLIST_SET = 'blocklist.set';
-export const BLOCKLIST_UPDATE = 'blocklist.update';
-
-export const CALL = 'call';
-
+export const LID_MAPPING_UPDATE = 'lid-mapping.update';
+export const CHATS_DELETE = 'chats.delete';
+export const PRESENCE_UPDATE = 'presence.update';
 export const CONTACTS_UPSERT = 'contacts.upsert';
 export const CONTACTS_UPDATE = 'contacts.update';
-
+export const MESSAGES_DELETE = 'messages.delete';
+export const MESSAGES_UPDATE = 'messages.update';
+export const MESSAGES_MEDIA_UPDATE = 'messages.medua-update';
+export const MESSAGES_UPSERT = 'messages.upsert';
+export const MESSAGES_REACTION = 'messages.reaction';
+export const MESSAGE_RECEIPT_UPDATE = 'message-receipt.update';
 export const GROUPS_UPSERT = 'groups.upsert';
 export const GROUPS_UPDATE = 'groups.update';
 export const GROUP_PARTICIPANTS_UPDATE = 'group-participants.update';
+export const GROUP_JOIN_REQUEST = 'group.join-request';
+export const BLOCKLIST_SET = 'blocklist.set';
+export const BLOCKLIST_UPDATE = 'blocklist.update';
+export const CALL = 'call';
+export const LABELS_EDIT = 'labels.edit';
+export const LABELS_ASSOCIATION = 'labels.association';
+export const NEWSLETTER_REACTION = 'newsletter.reaction';
+export const NEWSLETTER_VIEW = 'newsletter.view';
+export const NEWSLETTER_PARTICIPANTS_UPDATE = 'newsletter-participants.update';
+export const NEWSLETTER_SETTINGS_UPDATE = 'newsletter-settings.update';
 
-export const Events = {
-  CONNECTION_UPDATE: 'connection.update',
-  CREDS_UPDATE: 'creds.update',
-  MESSAGING_HISTORY_SET: 'messaging-history.set',
-  CHATS_UPSERT: 'chats.upsert',
-  CHATS_UPDATE: 'chats.update',
-  CHATS_PHONENUMBERSHARE: 'chats.phoneNumberShare',
-  CHATS_DELETE: 'chats.delete',
-  PRESENCE_UPDATE: 'presence.update',
-  CONTACTS_UPSERT: 'contacts.upsert',
-  CONTACTS_UPDATE: 'contacts.update',
-  MESSAGES_DELETE: 'messages.delete',
-  MESSAGES_UPDATE: 'messages.update',
-  MESSAGES_MEDIA_UPDATE: 'messages.media-update',
-  MESSAGES_UPSERT: 'messages.upsert',
-  MESSAGES_REACTION: 'messages.reaction',
-  MESSAGE_RECEIPT_UPDATE: 'message-receipt.update',
-  GROUPS_UPSERT: 'groups.upsert',
-  GROUPS_UPDATE: 'groups.update',
-  GROUP_PARTICIPANTS_UPDATE: 'group-participants.update',
-  GROUP_JOIN_REQUEST: 'group.join-request',
-  BLOCKLIST_SET: 'blocklist.set',
-  BLOCKLIST_UPDATE: 'blocklist.update',
-  CALL: 'call',
-}
+
+/**
+ * @readonly
+ * @enum {string}
+ */
+export const Events = Object.freeze({
+  CONNECTION_UPDATE: CONNECTION_UPDATE,
+  CREDS_UPDATE: CREDS_UPDATE,
+  MESSAGING_HISTORY_SET: MESSAGING_HISTORY_SET,
+  CHATS_UPSERT: CHATS_UPSERT,
+  CHATS_UPDATE: CHATS_UPDATE,
+  LID_MAPPING_UPDATE: LID_MAPPING_UPDATE,
+  CHATS_DELETE: CHATS_DELETE,
+  PRESENCE_UPDATE: PRESENCE_UPDATE,
+  CONTACTS_UPSERT: CONTACTS_UPSERT,
+  CONTACTS_UPDATE: CONTACTS_UPDATE,
+  MESSAGES_DELETE: MESSAGES_DELETE,
+  MESSAGES_UPDATE: MESSAGES_UPDATE,
+  MESSAGES_MEDIA_UPDATE: MESSAGES_MEDIA_UPDATE,
+  MESSAGES_UPSERT: MESSAGES_UPSERT,
+  MESSAGES_REACTION: MESSAGES_REACTION,
+  MESSAGE_RECEIPT_UPDATE: MESSAGE_RECEIPT_UPDATE,
+  GROUPS_UPSERT: GROUPS_UPSERT,
+  GROUPS_UPDATE: GROUPS_UPDATE,
+  GROUP_PARTICIPANTS_UPDATE: GROUP_PARTICIPANTS_UPDATE,
+  GROUP_JOIN_REQUEST: GROUP_JOIN_REQUEST,
+  BLOCKLIST_SET: BLOCKLIST_SET,
+  BLOCKLIST_UPDATE: BLOCKLIST_UPDATE,
+  CALL: CALL,
+  LABELS_EDIT: LABELS_EDIT,
+  LABELS_ASSOCIATION: LABELS_ASSOCIATION,
+  NEWSLETTER_REACTION: NEWSLETTER_REACTION,
+  NEWSLETTER_VIEW: NEWSLETTER_VIEW,
+  NEWSLETTER_PARTICIPANTS_UPDATE: NEWSLETTER_PARTICIPANTS_UPDATE,
+  NEWSLETTER_SETTINGS_UPDATE: NEWSLETTER_SETTINGS_UPDATE,
+});
