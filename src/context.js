4d2d75b0fd0d891e5d86378a38ecb596948a123b
ginkohub <ginkohub@gmail.com>
2026-05-02 22:05:10 +0800

    refactor: move function to classes

--- src/context.js
+++ src/context.js
@@ -82,25 +82,19 @@ export function extractTextContext(m) {
  */
 export class Ctx {
   /**
-   * @param {{handler: import('./handler.js').Handler, eventName: string, eventType: string, event: any}} opts
+   * @param {{handler: import('./handler.js').Handler, eventName: import('./const.js').Events, eventType: string, event: any}} opts
    */
   constructor({ handler, eventName, eventType, event }) {
     /** @returns {import('./handler.js').Handler} */
     this.handler = () => handler;
 
-    /** @returns {import('./plugin.js').Plugin|any} */
-    this.plugin = () => null;
-
-    /** @type {string|any} */
-    this.prefix = null;
-
     /** @returns {import('baileys').WASocket} */
     this.sock = () => handler?.client?.sock;
 
-    /** @type {string} */
+    /** @type {import('./const.js').Events} */
     this.eventName = eventName;
 
-    /** @type {import('baileys').WAMessage | any} */
+    /** @type {import('baileys').WAMessage|undefined} */
     this.event = event;
 
     /** @type {string} */
@@ -108,134 +102,11 @@ export class Ctx {
   }
 
   async init() {
-    /**
-     * @param {string} jid
-     * @returns (string|any)}
-     */
-    this.getName = (jid) => this.handler()?.getName(jid);
+    /** @returns {import('./plugin.js').Plugin|undefined} */
+    this.plugin = () => null;
 
-    /**
-     * @param {string} jid
-     * @param {import('baileys').AnyMessageContent} content
-     * @param {import('baileys').MessageGenerationOptions} options
-     * @returns {Promise<import('baileys').proto.WebMessageInfo>}
-     */
-    this.sendMessage = async (jid, content, options) =>
-      await this.handler()?.sendMessage(jid, content, options);
-
-    /**
-     * @param {string} jid
-     * @param {import('baileys').proto.IMessage} content
-     * @param {import('baileys').MessageRelayOptions} options
-     * @returns {Promise<string>}
-     */
-    this.relayMessage = async (jid, content, options) =>
-      await this.handler()?.relayMessage(jid, content, options);
-
-    /**
-     * @param {import('baileys').AnyMessageContent} content
-     * @param {import('baileys').MiscMessageGenerationOptions} options
-     * @returns {Promise<import('baileys').proto.IWebMessageInfo>}
-     */
-    this.reply = async (content, options) => {
-      if (!this.chat) throw new Error("chat jid not provided");
-      return await this.sendMessage(this.chat, content, options);
-    };
-
-    /**
-     * @param {import('baileys').proto.IMessage} content
-     * @param {import('baileys').MessageRelayOptions} options
-     * @returns {Promise<string>}
-     */
-    this.replyRelay = async (content, options) => {
-      if (!this.chat) throw new Error("chat jid not provided");
-      return await this.relayMessage(this.chat, content, options);
-    };
-
-    /**
-     * @param {string} jid - JID to download media from
-     * @param {string} emoji - Emoji to react with
-     * @param {import('baileys').WAMessageKey} key - Message key to react to
-     * @param {import('baileys').MessageGenerationOptions} [options] - Message options
-     * @returns {Promise<import('baileys').proto.IWebMessageInfo>}
-     */
-    this.reactIt = async (jid, emoji, key, options) =>
-      await this.sendMessage(
-        jid,
-        { react: { text: emoji, key: key } },
-        options,
-      );
-
-    /**
-     * @param {string} emoji - Emoji to react with
-     * @param {import('baileys').WAMessageKey} key - Message key to react to
-     * @returns {Promise<import('baileys').proto.IWebMessageInfo>}
-     */
-    this.react = async (emoji, key) =>
-      await this.reactIt(this.chat, emoji, key ?? this.key);
-
-    /**
-     * @param {import('baileys').ChatModification} mods
-     * @param {string} jid
-     */
-    this.chatModify = async (mods, jid) =>
-      await this.sock()?.chatModify(mods, jid);
-
-    /**
-     * @param {import('baileys').proto.IMessageKey[]} keys
-     * @returns {Promise<void>}
-     */
-    this.readMessages = async (keys) => await this.sock()?.readMessages(keys);
-
-    /**
-     * @param {string} lid
-     * @returns {string|any}
-     */
-    this.LIDToPN = async (lid) =>
-      jidNormalizedUser(
-        await this.sock().signalRepository.lidMapping.getPNForLID(lid),
-      );
-
-    /**
-     * @param {string} jid
-     * @returns {string|any}
-     */
-    this.PNToLID = async (jid) =>
-      jidNormalizedUser(
-        await this.sock().signalRepository.lidMapping.getLIDForPN(jid),
-      );
-
-    /**
-     * @param {string} text - Text to parse
-     */
-    this.parseText = (text) => {
-      this.text = text;
-
-      /* Parsing cmd */
-      if (text && text.length > 0) {
-        const splitted = text.split(" ");
-        /** @type {string} - With prefix */
-        this.pattern = splitted[0];
-
-        /** @type {string} - No prefixed */
-        this.cmd = this.pattern?.slice(this.prefix?.length ?? 1);
-
-        /** @type {string} */
-        this.args = splitted.slice(1)?.join(" ");
-
-        /** @type {boolean} */
-        this.isCMD = this.handler()?.isCMD(this.pattern);
-
-        if (this.args && this.args?.length > 0) {
-          try {
-            /** @type {import('minimist').ParsedArgs} */
-            this.argv = minimist(parseArgsStringToArgv(this.args));
-          } catch {
-            /* do nothing */
-          }
-        }
-      }
-    };
+    /** @type {string} */
+    this.prefix = '';
 
     /** @type {number} */
     this.timestamp = this.event?.messageTimestamp
@@ -432,74 +303,6 @@ export class Ctx {
       !this.type &&
       this.event?.messageStubParameters?.includes("Message absent from node");
 
-    /**
-     * @param {import('baileys').WAMessage} m
-     * @param {'buffer' | 'stream'} output
-     * @param {import('baileys').DownloadMediaOptions} options
-     * @returns {Promise<import('fs').ReadStream | Buffer>}
-     */
-    this.downloadIt = async (m, output, options) => {
-      if (!output || typeof output !== "string" || output.length === 0)
-        output = "buffer";
-      if (m?.message?.documentWithCaptionMessage)
-        m = m.message.documentWithCaptionMessage;
-      if (m?.message?.viewOnceMessage) m = m.message.viewOnceMessage;
-      return downloadMediaMessage(m, output, options);
-    };
-
-    /**
-     * @param {'buffer' | 'stream'} output
-     * @param {import('baileys').DownloadMediaOptions} options
-     * @returns {Promise<import('fs').ReadStream | Buffer>}
-     */
-    this.download = async (output, options) => {
-      return this.downloadIt({ message: this.message }, output, options);
-    };
-
-    /**
-     * @param {'buffer' | 'stream'} output
-     * @param {import('baileys').DownloadMediaOptions} options
-     * @returns {Promise<import('fs').ReadStream | Buffer>}
-     */
-    this.downloadQuoted = async (output, options) => {
-      if (!this.quotedMessage) return;
-      return this.downloadIt({ message: this.quotedMessage }, output, options);
-    };
-
-    /**
-     * @param {...JIDBy} by
-     * @returns {string[]}
-     */
-    this.parseJIDs = (...by) => {
-      if (!by || by.length === 0)
-        by = [JIDBy.Participant, JIDBy.Mentions, JIDBy.Text];
-
-      let jids = [];
-
-      if (by?.includes(JIDBy.Participant)) {
-        if (this.participant) jids.push(this.participant);
-      }
-      if (by?.includes(JIDBy.Mentions)) {
-        if (this.mentionedJid) jids.push(...this.mentionedJid);
-      }
-
-      if (by?.includes(JIDBy.Text)) {
-        const uncat = this.argv?._?.join(" ") ?? "";
-        const matches = uncat.matchAll(PHONE_REGEX);
-
-        for (const match of matches) {
-          const cleanNumber = match[1].replace(/[^\d]/g, "");
-          if (cleanNumber.length > 6 && cleanNumber.length < 16) {
-            const suffix = this.addressingMode === "lid" ? "@lid" : S_WHATSAPP_NET;
-            jids.push(`${cleanNumber}${suffix}`);
-          }
-        }
-      }
-
-      /* remove duplicate in jids */
-      return [...new Set(jids)];
-    };
-
     /** @returns {string} */
     this.user = () => this.handler().userManager?.getUser(this.senderJid);
   }
@@ -513,4 +316,214 @@ export class Ctx {
     Object.assign(cloned, this);
     return cloned;
   }
+
+  /**
+   * @param {string} jid
+   * @returns {string}
+   */
+  getName(jid) {
+    return this.handler()?.getName(jid);
+  }
+
+  /**
+   * @param {string} jid
+   * @param {import('baileys').AnyMessageContent} content
+   * @param {import('baileys').MessageGenerationOptions} options
+   * @returns {Promise<import('baileys').proto.WebMessageInfo>}
+   */
+  async sendMessage(jid, content, options) {
+    return await this.handler()?.sendMessage(jid, content, options);
+  }
+
+  /**
+   * @param {string} jid
+   * @param {import('baileys').proto.IMessage} content
+   * @param {import('baileys').MessageRelayOptions} options
+   * @returns {Promise<string>}
+   */
+  async relayMessage(jid, content, options) {
+    return await this.handler()?.relayMessage(jid, content, options);
+  }
+
+  /**
+   * @param {import('baileys').AnyMessageContent} content
+   * @param {import('baileys').MiscMessageGenerationOptions} options
+   * @returns {Promise<import('baileys').proto.IWebMessageInfo>}
+   */
+  async reply(content, options) {
+    if (!this.chat) throw new Error("chat jid not provided");
+    return await this.sendMessage(this.chat, content, options);
+  }
+
+  /**
+   * @param {import('baileys').proto.IMessage} content
+   * @param {import('baileys').MessageRelayOptions} options
+   * @returns {Promise<string>}
+   */
+  async replyRelay(content, options) {
+    if (!this.chat) throw new Error("chat jid not provided");
+    return await this.relayMessage(this.chat, content, options);
+  }
+
+  /**
+   * @param {string} jid - JID to download media from
+   * @param {string} emoji - Emoji to react with
+   * @param {import('baileys').WAMessageKey} key - Message key to react to
+   * @param {import('baileys').MessageGenerationOptions} [options] - Message options
+   * @returns {Promise<import('baileys').proto.IWebMessageInfo>}
+   */
+  async reactIt(jid, emoji, key, options) {
+    return await this.sendMessage(
+      jid,
+      { react: { text: emoji, key: key } },
+      options,
+    );
+  }
+
+  /**
+   * @param {string} emoji - Emoji to react with
+   * @param {import('baileys').WAMessageKey} key - Message key to react to
+   * @returns {Promise<import('baileys').proto.IWebMessageInfo>}
+   */
+  async react(emoji, key) {
+    return await this.reactIt(this.chat, emoji, key ?? this.key);
+  }
+
+  /**
+   * @param {import('baileys').ChatModification} mods
+   * @param {string} jid
+   */
+  async chatModify(mods, jid) {
+    return await this.sock()?.chatModify(mods, jid);
+  }
+
+  /**
+   * @param {import('baileys').proto.IMessageKey[]} keys
+   * @returns {Promise<void>}
+   */
+  async readMessages(keys) {
+    return await this.sock()?.readMessages(keys);
+  }
+
+  /**
+   * @param {string} lid
+   * @returns {Promise<string>}
+   */
+  async LIDToPN(lid) {
+    return jidNormalizedUser(
+      await this.sock().signalRepository.lidMapping.getPNForLID(lid),
+    );
+  }
+
+  /**
+   * @param {string} jid
+   * @returns {Promise<string>}
+   */
+  async PNToLID(jid) {
+    return jidNormalizedUser(
+      await this.sock().signalRepository.lidMapping.getLIDForPN(jid),
+    );
+  }
+
+  /**
+   * @param {string} text - Text to parse
+   */
+  parseText(text) {
+    this.text = text;
+
+    /* Parsing cmd */
+    if (text && text.length > 0) {
+      const splitted = text.split(" ");
+      /** @type {string} - With prefix */
+      this.pattern = splitted[0];
+
+      /** @type {string} - No prefixed */
+      this.cmd = this.pattern?.slice(this.prefix?.length ?? 1);
+
+      /** @type {string} */
+      this.args = splitted.slice(1)?.join(" ");
+
+      /** @type {boolean} */
+      this.isCMD = this.handler()?.isCMD(this.pattern);
+
+      if (this.args && this.args?.length > 0) {
+        try {
+          /** @type {import('minimist').ParsedArgs} */
+          this.argv = minimist(parseArgsStringToArgv(this.args));
+        } catch {
+          /* do nothing */
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {import('baileys').WAMessage} m
+   * @param {'buffer' | 'stream'} output
+   * @param {import('baileys').DownloadMediaOptions} options
+   * @returns {Promise<import('fs').ReadStream | Buffer>}
+   */
+  async downloadIt(m, output, options) {
+    if (!output || typeof output !== "string" || output.length === 0)
+      output = "buffer";
+    if (m?.message?.documentWithCaptionMessage)
+      m = m.message.documentWithCaptionMessage;
+    if (m?.message?.viewOnceMessage) m = m.message.viewOnceMessage;
+    return downloadMediaMessage(m, output, options);
+  }
+
+  /**
+   * @param {'buffer' | 'stream'} output
+   * @param {import('baileys').DownloadMediaOptions} options
+   * @returns {Promise<import('fs').ReadStream | Buffer>}
+   */
+  download(output, options) {
+    return this.downloadIt({ message: this.message }, output, options);
+  }
+
+  /**
+   * @param {'buffer' | 'stream'} output
+   * @param {import('baileys').DownloadMediaOptions} options
+   * @returns {Promise<import('fs').ReadStream | Buffer>}
+   */
+  downloadQuoted(output, options) {
+    if (!this.quotedMessage) return;
+    return this.downloadIt({ message: this.quotedMessage }, output, options);
+  }
+
+  /**
+   * @param {...JIDBy} by
+   * @returns {string[]}
+   */
+  parseJIDs(...by) {
+    if (!by || by.length === 0)
+      by = [JIDBy.Participant, JIDBy.Mentions, JIDBy.Text];
+
+    let jids = [];
+
+    if (by?.includes(JIDBy.Participant)) {
+      if (this.participant) jids.push(this.participant);
+    }
+    if (by?.includes(JIDBy.Mentions)) {
+      if (this.mentionedJid) jids.push(...this.mentionedJid);
+    }
+
+    if (by?.includes(JIDBy.Text)) {
+      const uncat = this.argv?._?.join(" ") ?? "";
+      const matches = uncat.matchAll(PHONE_REGEX);
+
+      for (const match of matches) {
+        const cleanNumber = match[1].replace(/[^\d]/g, "");
+        if (cleanNumber.length > 6 && cleanNumber.length < 16) {
+          const suffix = this.addressingMode === "lid" ? "@lid" : S_WHATSAPP_NET;
+          jids.push(`${cleanNumber}${suffix}`);
+        }
+      }
+    }
+
+    /* remove duplicate in jids */
+    return [...new Set(jids)];
+  }
+
+
 }
